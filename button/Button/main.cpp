665420e7370dea1c39f124e4f436ffc74f415047
songwentao <614425032@qq.com>
2019-04-22 16:06:38 +0800

    创建QtQuickApplication工程,show出可视化对象Item
    该工程展现了QQmlApplicationEngine和QQuickView的不同，同时在自定义属性的方法中，QtObject内定义一系列属性的特点。

---
+++ button/Button/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <QGuiApplication>
+//#include <QQmlApplicationEngine>
+#include <QQuickView>
+#include <QQuickItem>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+//    QQmlApplicationEngine engine;
+//    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    QQuickView *view = new QQuickView;
+    view->setSource(QUrl::fromLocalFile(":/main.qml"));
+    view->show();
+
+    QQuickItem *item = view->rootObject()->findChild<QQuickItem*>("mainText");
+     if (item)
+         item->setProperty("color", QColor(Qt::blue));
+
+
+    return app.exec();
+}
---
+++ button/Button/main.qml
@@ -0,0 +1,29 @@
+﻿import QtQuick 2.5
+//import QtQuick.Window 2.2
+
+//Window {
+//    visible: true
+//    width: 640
+//    height: 480
+//    title: qsTr("Hello World")
+
+//}
+
+Item
+{
+    width: 100
+    height: width
+    Text {
+        text: attributes.name
+        anchors.centerIn: parent
+        color: "red"
+        objectName: "mainText"
+    }
+//QtObject封装一批自定义的属性，这些属性在QtObject之外只能通过唯一标识id访问其属性
+    QtObject {
+        id: attributes
+        property string name: "hello"
+        property int size
+        property variant attributes
+    }
+}
